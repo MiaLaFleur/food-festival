b12a3bac8af715f99bc8ae8187c62e996b173e1f
MiaLaFleur <mia.lafleur@yahoo.com>
2022-08-12 07:28:24 -0500

    implement webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/schedule.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path].[name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+        })
+    ],
+    mode: 'development'
+};
